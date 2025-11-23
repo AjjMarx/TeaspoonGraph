3e67adb0d2445af4c6726e20ad048bbcecae793e
Alexa <ajjmarx@umich.edu>
2025-11-23 15:11:54 -0500

    Colors added to blocks

--- Blocks.js
+++ Blocks.js
@@ -1,71 +1,93 @@
-const Palette = {
-	Red : "#F75454",
-	Orange : "#E6A45A",
-	Yellow : "#E3D12D",
-	Lime : "#9FEB81", 
-	Green : "#3EB56F",
-	Aqua : "#5ED6D0",
-	Blue : "#39A2E3",
-	Purple : "#9F6FF2",
-	Pink : "#F299E3" 
-}
+const Palette = [
+	["#F75454", "Red"],
+	["#E6A45A", "Orange"],
+	["#E3D12D", "Yellow"],
+	["#9FEB81", "Lime"], 
+	["#3EB56F", "Green"],
+	["#5ED6D0", "Aqua"],
+	["#39A2E3", "Blue"],
+	["#9F6FF2", "Purple"],
+	["#F299E3", "Pink"] 
+];
+
+const PaletteReverse = {
+	Red : 0,
+	Orange : 1,
+	Yellow : 2,
+	Lime : 3,
+	Green : 4,
+	Aqua : 5,
+	Blue : 6,
+	Purple : 7,
+	Pink: 8
+};
 
 class Block {
 	constructor() {
 		this.container = document.createElement("div");
+		this.hitBox = document.createElement("div");
 
-		console.log("Block generated");
-
-		this.container.style.width = "150px";
-		this.container.style.height = "100px";
-		this.container.style.margin = "30px";
-
+		this.container.id = "Block";
+		this.container.style.position = "absolute";
+		this.container.appendChild(this.hitBox);
+		this.hitBox.style.position = "relative";
 
 		this.style = {
 			width : 50,
 			height : 32,
 			left : 0,
-			right : 0,
-			color : "Red",
+			top : 0,
+			color : "Orange",
 			size : null,
 			edgeShapes : ["Straight", "Puzzle", "Straight", "Puzzle"],
 			text : "Trade $1 $2 For $3 $4"
 		};
 
-		this.style.width = this.style.text.length * 9 + 24;
+		this.style.width = this.style.text.length * 8 + 24;
 	
 		this.container.innerHTML = this.generateSVG();
+
+		//this.update();
 	}
 
 	update() {
-		
+		this.style.width = this.style.text.length * 8 + 24;
+		this.container.style.left = String(this.style.left - 8) + "px";
+		this.container.style.top = String(this.style.top - 8) + "px";
+		this.container.style.width = String(16 + this.style.width) + "px";
+		this.container.style.height = String(16 + this.style.height) + "px";
+		this.hitBox.style.left = "8px";
+		this.hitBox.style.top = "8px";
+		this.hitBox.style.width = String(this.style.width) + "px";
+		this.hitBox.style.height = String(this.style.height) + "px";
+		this.container.innerHTML = this.generateSVG();
+	}
+
+	getBottom() {
+		return this.style.top + this.style.height + 1;
 	}
 
 	generateSVG() {
-		//console.log(this.style);
 		let temp = `<svg width="` + (16 + this.style.width) + `" height="` + (16 + this.style.height) + `">`;
-		//temp += `<rect x="8" y="8" width="` + this.style.width  + `" height="` + this.style.height + `" stroke="red" stroke-width="1" fill="white"/>`;
 		
 		temp += `<polygon points="`;
 		
 		temp += `8,` + (8 + this.style.height) + ` `;
 		temp += `8,8 `;
 		if (this.style.edgeShapes[1] == "Straight") {
-			console.log("Top:Straight");
 			temp += (8 + this.style.width) + `,8 `;	
 		} else if (this.style.edgeShapes[1] == "Puzzle") {
-			console.log("Top:Puzzle");
 			temp += `16,8 24,15 32,15 40,8 ` + (8 + this.style.width) + `,8 `;	
 		}		
 		temp += (8 + this.style.width) + `,` + (8 + this.style.height) + ` `;
 		if (this.style.edgeShapes[3] == "Straight") {
 			temp += `8,` + (8 + this.style.height) + ` `;
 		} else if (this.style.edgeShapes[3] == "Puzzle") {
-			temp += '38,' + (8 + this.style.height) + ' 31,' + (13 + this.style.height) + ' 25,' + (13 + this.style.height) + ' 18,' + (8 + this.style.height)  + ` 8,` + (8 + this.style.height) + ` `;
+			temp += '39.3,' + (8 + this.style.height) + ' 31.3,' + (15 + this.style.height) + ' 24.7,' + (15 + this.style.height) + ' 16.7,' + (8 + this.style.height)  + ` 8,` + (8 + this.style.height) + ` `;
 		}
-			
-		temp += `" fill="` + Palette[this.style.color]  + `"/>`;
-		temp += `<text fill="#ffffff" font-size="18" ` + `x="16" y="` + (16 +this.style.height/2)+ `"` + `>` + this.style.text + ` </text>`;
+		console.log(this.style.color);
+		temp += `" fill="` + Palette[PaletteReverse[this.style.color]][0]  + `"/>`;
+		temp += `<text fill="#ffffff" font-size="16" ` + `x="16" y="` + (14 +this.style.height/2)+ `"` + `>` + this.style.text + ` </text>`;
 		temp += `</svg>`;
 		return temp;
 
--- Lesson.js
+++ Lesson.js
@@ -7,7 +7,7 @@ async function LoadLesson() {
 		console.log(lessonData["Vertices"]);
 
 		for (vert of lessonData["Vertices"]) {
-			console.log(vert);
+			//console.log(vert);
 			let ph = 3.14159* (vert.Latitude)/180;
 			let th = -3.14159*(vert.Longitude)/180 + 1.5707;
 			points.push([Math.cos(th) * Math.cos(ph), Math.sin(th) * Math.cos(ph), Math.sin(ph), 0, 0, 0]);
@@ -26,13 +26,30 @@ async function LoadLesson() {
 		playground.style.width = "300px";
 		playground.style.height = "300px";
 
-		let testBlock = new Block();
 
-		playground.appendChild(testBlock.container);
+		let blocks = [];
+		blocks[-1] = new Block();
+		playground.appendChild(blocks[-1].container);
+		blocks[-1].style.left = 100;
+		blocks[-1].style.top = 100;
+		blocks[-1].update();
+		
+		for (i in lessonData["Code"]["Types"]) { 
+			blocks[i] = new Block();
+
+			playground.appendChild(blocks[i].container);
+			blocks[i].style.left = 100;
+			blocks[i].style.top = blocks[i-1].getBottom();
+			console.log(lessonData["Code"]["Types"][i]["Color"]);
+			blocks[i].style.color = lessonData["Code"]["Types"][i]["Color"];
+			blocks[i].style.text = lessonData["Code"]["Types"][i]["Name"];
+			console.log(blocks[i].style.color);
+			blocks[i].update();
+		}
 
 		requestAnimationFrame(renderGlobe);
 	} catch(err) {
-		console.log("Lesson data could not be loaded.");
+		console.log("Lesson data could not be loaded:\n" + err);
 	}
 }
 
